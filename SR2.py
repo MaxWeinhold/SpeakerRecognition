e1bb6a9497986bc93ce17e26530a076cb988e79b
Maximilian Weinhold <78552945+MaxWeinhold@users.noreply.github.com>
2022-07-28 19:04:15 +0200

    Data Set Summary

--- SR2.py
+++ SR2.py
@@ -15,43 +15,53 @@ import numpy as np
 #Bibliothek um CSV Dateien zu speichen
 import pandas as pd
 
-Sound_Files = [r"D:\STUDIUM\Münster\VPRonaRaspberryPi\EmoDB\wav\09b09Nd.wav", 
-               r"D:\STUDIUM\Münster\VPRonaRaspberryPi\EmoDB\wav\03a02Wb.wav", 
-               r"D:\STUDIUM\Münster\VPRonaRaspberryPi\EmoDB\wav\08b01Wa.wav",
-               r"D:\STUDIUM\Münster\VPRonaRaspberryPi\EmoDB\wav\08b02Tc.wav",
-               r"D:\STUDIUM\Münster\VPRonaRaspberryPi\EmoDB\wav\11a05Ad.wav"
+#Dateinamen
+Sound_Files = [r"\09b09Nd", 
+               r"\03a02Wb", 
+               r"\08b01Wa",
+               r"\08b02Tc",
+               r"\11a05Ad"
                ]
 
+i=0
 
 for audio_file in Sound_Files:
-    print(audio_file)
     
-    ipd.Audio(audio_file,) 
+    #Fortschrittsanzeige
+    i=i+1
+    print(str(i/len(Sound_Files)*100)+" %")
+    
+    #Dateipfad
+    str1 = "D:\STUDIUM\Münster\VPRonaRaspberryPi\EmoDB\wav"
+    str2 = ".wav"
+    #str3 = audiofile
+    wavFileName= str1 + audio_file + str2
+    
+    ipd.Audio(wavFileName,) 
 
     # load audio files with librosa
-    signal, sr = librosa.load(audio_file)
+    signal, sr = librosa.load(wavFileName)
 
     #Extracting MFCCs
     mfccs = librosa.feature.mfcc(y=signal, n_mfcc=13, sr=sr)
     mfccs.shape
 
     #Visualising MFCCs
-    plt.figure(figsize=(25, 10))
-    librosa.display.specshow(mfccs, 
-                             x_axis="time", 
-                             sr=sr)
-    plt.colorbar(format="%+2.f")
-    plt.show()
+    #plt.figure(figsize=(25, 10))
+    #librosa.display.specshow(mfccs, 
+    #                         x_axis="time", 
+    #                         sr=sr)
+    #plt.colorbar(format="%+2.f")
+    #plt.show()
     
     #Teste ob der Frequenz Output die Mindestgröße hat
-    print(mfccs.size/13)
-    
     if mfccs.size/13 > 50:
         #Alle Outputs auf den selben Stichproben Umfang bringen
         mfccs_resized=np.resize(mfccs,(13,50))
     
         #Speichere MFCCs Output als CSV Datei
         df = pd.DataFrame(mfccs_resized)
-        df.to_csv(r'D:\ComicandSonsProductions\GameJam1\SpeakerRecognition\TestData\TestData.csv')
-        #mfccs.to_csv (r'D:\ComicandSonsProductions\GameJam1\SpeakerRecognition\TestData', index = False, header=True)
+        #Dateipfad
+        filename="D:\ComicandSonsProductions\GameJam1\SpeakerRecognition\TestData"+audio_file+".csv"
+        df.to_csv(filename)
     
